f62bacc4a7697bc9ca10dafc66ebe106739c76ea
Caique Souza Fonseca <c.souzaf@hotmail.com>
2024-12-14 22:56:39 -0300

    Add s3 to lambda aws

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,37 +8,64 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
     
-    private final ObjectMapper objectMapper = new ObjectMapper();
+        private final ObjectMapper objectMapper = new ObjectMapper();
+        
+        private final S3Client s3Client = S3Client.builder().build();
+        
 
-    @Override
-    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
-       
-        String body = input.get("body").toString();
+        @Override
+        public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        
+            String body = input.get("body").toString();
 
-        Map<String, String> bodyMap;
-        
-        try {
+            Map<String, String> bodyMap;
             
-            bodyMap = objectMapper.readValue(body, Map.class);
+            try {
+                
+                bodyMap = objectMapper.readValue(body, Map.class);
+                
+            } catch (Exception e) {
+                
+            throw new RuntimeException("Error parsing JSON body" + e.getMessage(), e);
+            }
             
-        } catch (Exception e) {
-        	
-           throw new RuntimeException("Error parsing JSON body" + e.getMessage(), e);
-        }
+            String originalURL = bodyMap.get("originalUrl");
+            String expirationTime = bodyMap.get("expirationTime");
+            
+            String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+            
+            long expirationTimeInSeconds = Long.parseLong(expirationTime);
+            
+            UrlData urlData = new UrlData(originalURL, expirationTimeInSeconds);
+            
+            try {
+            	
+            	String urlDataJson = objectMapper.writeValueAsString(originalURL);
+            	
+            	PutObjectRequest request = PutObjectRequest.builder()
+            			.bucket("s3-java-aws-lambda")
+            			.key(shortUrlCode + ".json")
+            			.build();
+            			
+            	s3Client.putObject(request, RequestBody.fromString(urlDataJson));         	
+            	
+			} catch (Exception e) {
+				throw new RuntimeException("Error saving Url data to S3: " + e.getMessage(), e);
+			}
+            
+            Map<String, String> response = new HashMap<String, String>();
+            
+            response.put("code", shortUrlCode);
+            
+            return response;
         
-        String originaURL = bodyMap.get("originalUrl");
-        String expirationTime = bodyMap.get("expirationTime");
-        
-        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
-        
-        Map<String, String> response = new HashMap<String, String>();
-        
-        response.put("code", shortUrlCode);
-        
-        return response;
-       
     }
+  
     
 }
---
+++ src/main/java/com/example/UrlData.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UrlData {
+	
+	private String originalUrl;
+	
+	private long expirationTime;
+}
